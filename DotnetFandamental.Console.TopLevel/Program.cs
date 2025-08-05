86a4dc9d4e8d703cc4ae6c1186cfc42c3b027e55
Administrator <Administrator@PC-12>
2025-08-05 19:57:15 +0330

    Add project files.

---
+++ DotnetFandamental.Console.TopLevel/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is your name?");
+Console.ReadLine();
+Console.WriteLine("Thank you!");
+
+//DataType اسم ظرف داده =  "";
+//Predefine datatype
+int age = 25;
+byte b = 245;
+bool isRegisted = true;
+DateTime dateTime;
+char agree = 'N';
+string firstName = "parham"; // Inital value
+string lastName;
+lastName = "darvishi";
---
+++ DotnetFandamental.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DotnetFandamental;
+
+internal class Program
+{
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is your first name?");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string lastName = Console.ReadLine();
+
+        string fullName = firstName + " " + lastName;
+
+        lastName = lastName + 2;
+
+        Console.WriteLine("Thank you! " + fullName);
+    }
+}
